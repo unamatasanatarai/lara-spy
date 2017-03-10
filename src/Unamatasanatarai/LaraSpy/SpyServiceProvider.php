044325852d1292fd74b842c62128d28e01432076
piotr grabowski <piotr.grabowski@performancehorizon.com>
2017-03-10 12:20:03 +0100

    qf

--- src/Unamatasanatarai/LaraSpy/SpyServiceProvider.php
+++ src/Unamatasanatarai/LaraSpy/SpyServiceProvider.php
@@ -20,25 +20,7 @@ class SpyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ( ! $this->migrationHasAlreadyBeenPublished() ) {
-            // Publish migration
-            $timestamp = date('Y_m_d_His', time());
-
-            $this->publishes([
-                __DIR__ . "/migrations/create_lara_spy_table.stub" => database_path("/migrations/{$timestamp}_create_lara_spy_table.php"),
-            ], 'migrations');
-        }
-    }
-
-
-    /**
-     * @return bool
-     */
-    protected function migrationHasAlreadyBeenPublished()
-    {
-        $files = glob(database_path('/migrations/*_create_lara_spy_table.php'));
-
-        return count($files) > 0;
+        $this->loadMigrationsFrom(__DIR__ . '/migrations/');
     }
 
 
